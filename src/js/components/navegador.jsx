0e95f031225458505ff7d1383e073b07c4fd1773
José Isidro <isidro107109@gmail.com>
2025-10-28 19:43:03 +0000

    terminado

--- src/js/components/navegador.jsx
+++ src/js/components/navegador.jsx
@@ -2,25 +2,25 @@ import React from "react";
 
 export default function Navegador() {
     return (
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="تبديل التنقل">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="تبديل التنقل">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav ms-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <ul className="navbar-nav ms-auto mb-2 mb-md-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Service</a>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Service</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
                         </li>
                     </ul>
                 </div>
