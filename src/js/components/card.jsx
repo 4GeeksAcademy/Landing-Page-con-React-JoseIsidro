933b856b213f7737b33fdfd7de4c343dc03efd5b
José Isidro <isidro107109@gmail.com>
2025-10-28 19:42:10 +0000

    proyecto terminado

--- src/js/components/card.jsx
+++ src/js/components/card.jsx
@@ -4,22 +4,26 @@ import React from "react";
 export default function PCard() {
     return (
         <div className="row row-cols-1 row-cols-md-4 px-1">
-            <Card />
-            <Card />
-            <Card />
+            <Card titulo='Primer titulo' />
+            <Card titulo='Segundo titulo' />
+            <Card titulo='Tercer titulo' />
             <Card />
         </div>
     )
 }
 
-function Card() {
+function Card({ titulo }) {
     return (
         <div className="col mb-3">
             <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
+                <img src="/src/img/rigo-baby.jpg" className="card-img-top" alt="..." />
+                <div className="card-body text-center m-3">
+                    <h5 className="card-title">{titulo ? titulo : 'Titulo por Defecto'}</h5>
                     <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+                </div>
+                <div class="card-footer text-body-secondary text-center ">
+                    <a href="#" class="btn btn-primary m-2 ">Go somewhere</a>
                 </div>
             </div>
         </div>
--- src/js/components/navegador.jsx
+++ src/js/components/navegador.jsx
@@ -2,39 +2,27 @@ import React from "react";
 
 export default function Navegador() {
     return (
-        <nav className="navbar navbar-expand-lg fixed-top  bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand text-light" href="#">Navbar scroll</a>
-                <button className="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon text-light"></span>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Start Bootstrap</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="تبديل التنقل">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarScroll">
-                    <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll " style={{"--bs-scroll-height": "100px"}}>
-                        <li className="nav-item ">
-                            <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-light" href="#">Link</a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Link
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Service</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled text-light" aria-disabled="true">Link</a>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success text-light" type="submit">Search</button>
-                    </form>
                 </div>
             </div>
         </nav>
